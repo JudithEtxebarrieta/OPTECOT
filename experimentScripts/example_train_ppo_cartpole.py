2696db58e5d27f069986c11582ad53e5e9aa4463
EtorArza <etorarza@gmail.com>
2022-11-02 17:15:43 +0100

    working train with different accuracy

--- experimentScripts/example_train_ppo_cartpole.py
+++ experimentScripts/example_train_ppo_cartpole.py
@@ -3,12 +3,12 @@ import gym
 import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
+from tqdm import tqdm as tqdm
+import time
 
 
 # Based on https://colab.research.google.com/github/Stable-Baselines-Team/rl-colab-notebooks/blob/sb3/stable_baselines_getting_started.ipynb
 
-env = gym.make('CartPole-v1')
-model = PPO(MlpPolicy, env, verbose=1)
 
 
 def evaluate(model, env, n_eval_episodes=100, deterministic=True):
@@ -35,16 +35,40 @@ def evaluate(model, env, n_eval_episodes=100, deterministic=True):
 
     return np.mean(all_episode_rewards), np.std(all_episode_rewards)
 
+env = gym.make('CartPole-v1')
+
+
 # Use a separate environement for evaluation
 eval_env = gym.make('CartPole-v1')
 
-# Evaluate random Agent, before training
-mean_reward, std_reward = evaluate(model, eval_env, n_eval_episodes=100)
+
+n_samples_reward = 100
+default_tau = 0.02
+default_max_episode_steps = 500
+for accuracy in tqdm([1.0, 0.8, 0.6, 0.4]):
+    # tau is the number of seconds between each frame
+    env.env.tau = default_tau / accuracy
+
+    # After max_episode_steps steps, stop simulation.
+    env.env.spec.max_episode_steps = int(default_max_episode_steps * accuracy)
+    env.env._max_episode_steps = int(default_max_episode_steps * accuracy)
 
 
-model.learn(total_timesteps=10000)
-mean_reward, std_reward = evaluate(model, eval_env, n_eval_episodes=100, deterministic=True)
-print(mean_reward)
+    t = time.time()
+    
+
+    model = PPO(MlpPolicy, env, verbose=1)
+    model.learn(total_timesteps=10000)
+    mean_reward, std_reward = evaluate(model, eval_env, n_eval_episodes=100, deterministic=True)
+    print(mean_reward)
+
+
+    elapsed = time.time() - t
+
+
+
+
+
 
 # # Save the model
 # model.save("CartPole.PPOmodel")
