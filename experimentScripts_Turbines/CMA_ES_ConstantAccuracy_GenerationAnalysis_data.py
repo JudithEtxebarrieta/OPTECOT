ec4c5d37053abfbc5fdb408162f830a3f6277485
JudithEtxebarrieta <judithetxebarrieta@gmail.com>
2022-12-21 10:47:10 +0100

    add new experiments with their results

---
+++ experimentScripts_Turbines/CMA_ES_ConstantAccuracy_GenerationAnalysis_data.py
@@ -0,0 +1,248 @@
+#==================================================================================================
+# LIBRERÍAS
+#==================================================================================================
+import numpy as np
+import matplotlib as mpl
+import scipy as sc
+import matplotlib.pyplot as plt
+import scipy.integrate as scint
+import time
+import os
+from scipy.interpolate import interp1d
+from scipy.interpolate import LSQUnivariateSpline as lsqus
+from scipy.integrate import simpson
+from operator import itemgetter
+import openpyxl
+import pickle
+import copy
+from functools import partial
+from scipy.interpolate import interp1d
+from typing import Union
+import cma
+
+import sys
+sys.path.append("OptimizationAlgorithms_KONFLOT/packages")
+
+import turbine_classes
+import MathTools as mt
+import time
+
+from cma.utilities import utils 
+import pandas as pd
+from tqdm import tqdm
+
+#==================================================================================================
+# CLASES
+#==================================================================================================
+
+class stopwatch:
+    
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.start_t = time.time()
+        self.pause_t=0
+
+    def pause(self):
+        self.pause_start = time.time()
+        self.paused=True
+
+    def resume(self):
+        if self.paused:
+            self.pause_t += time.time() - self.pause_start
+            self.paused = False
+
+    def get_time(self):
+        return time.time() - self.start_t - self.pause_t
+
+#==================================================================================================
+# FUNCIONES
+#==================================================================================================
+def define_bounds():
+
+	# Definir rangos de los parámetros que definen el diseño de la turbina.
+	sigma_hub = [0.4, 0.7]# Hub solidity gene.
+	sigma_tip = [0.4, 0.7]# Tip solidity gene.
+	nu = [0.4, 0.75] # Hub-to-tip-ratio gene.
+	tip_clearance=[0,3]# Tip-clearance gene.	  
+	airfoil_dist = np.arange(0, 27)# Airfoil dist. gene.  
+
+	# Array con los rangos.
+	bounds=np.array([
+	[sigma_hub[0]    , sigma_hub[1]],
+	[sigma_tip[0]    , sigma_tip[1]],
+	[nu[0]           , nu[1]],
+	[tip_clearance[0], tip_clearance[1]],
+	[0               , 26]
+	])
+
+	return bounds
+
+def build_constargs_dict(N):
+	# Definir parámetros constantes.
+	omega = 2100# Rotational speed.
+	rcas = 0.4# Casing radius.
+	airfoils = ["NACA0015", "NACA0018", "NACA0021"]# Set of possible airfoils.
+	polars = turbine_classes.polar_database_load(filepath="OptimizationAlgorithms_KONFLOT/", pick=False)# Polars.
+	cpobjs = [933.78, 1089.41, 1089.41, 1011.59, 1011.59, 1011.59, 933.78, 933.78, 933.78, 855.96]# Target dumping coefficients.
+	devobjs = [2170.82, 2851.59, 2931.97, 2781.80, 2542.296783, 4518.520988, 4087.436172, 3806.379812, 5845.986619, 6745.134759]# Input sea-state standard pressure deviations.
+	weights = [0.1085, 0.1160, 0.1188, 0.0910, 0.0824, 0.1486, 0.0882, 0.0867, 0.0945, 0.0652]# Input sea-state weights.
+	Nmin = 1000#Max threshold rotational speeds
+	Nmax = 3200#Min threshold rotational speeds
+
+	# Construir el diccionario que necesita la función fitness
+	constargs = {"N": N,
+		     "omega": omega,
+		     "rcas": rcas,
+		     "airfoils": airfoils,
+		     "polars": polars,
+		     "cpobjs": cpobjs,
+		     "devobjs": devobjs,
+		     "weights": weights,
+		     "Nmin": Nmin,
+		     "Nmax": Nmax,
+		     "Mode": "mono"}
+
+	return constargs
+
+def fitness_function(turb_params,N=50):
+
+    # Construir diccionario de parámetros constantes.
+    constargs=build_constargs_dict(N)
+
+    # Crear turbina instantantanea.
+    os.chdir('OptimizationAlgorithms_KONFLOT')
+    turb = turbine_classes.instantiate_turbine(constargs, turb_params)
+    os.chdir('../')	
+
+    # Calcular evaluación.
+    if N==default_N:
+        sw_stop.resume()
+    global sw_eval_time
+    sw_eval_time=stopwatch()
+    scores = turbine_classes.fitness_func(constargs=constargs, turb=turb, out='brfitness')
+    sw_eval_time.pause()
+
+    if N==default_N:
+        sw_stop.pause()
+
+    return -scores[1] 
+
+def scale_x(x,bounds):
+    return x * (bounds[:,1] - bounds[:,0]) + bounds[:,0]
+
+def transform_turb_params(x, blade_number,bounds):
+    scaled_x = scale_x(x,bounds)
+    return [blade_number]+list(scaled_x[:-1])+[round(scaled_x[-1])]
+
+
+def evaluate(blade_number,bounds,seed):
+
+    # Inicializar CMA-ES
+    np.random.seed(seed)
+    es = cma.CMAEvolutionStrategy(np.random.random(5), 0.33,inopts={'bounds': [0, 1],'seed':seed,'popsize':10})
+
+    # Inicializar contadores de tiempo.
+    global sw_stop
+    sw_stop = stopwatch()
+    sw_stop.pause()
+
+    # Evaluar los diseños de las generaciones con diferentes accuracys para N hasta agotar el tiempo
+    # máximo definido para el accuracy máximo.
+    n_gen=0
+    while not es.stop():
+
+        # Nueva generación.
+        n_gen+=1
+        solutions = es.ask()
+
+        # Inicializar número de evaluaciones.
+        n_eval=0
+
+        # Evaluar nuevas soluciones e ir guardando tiempos.
+        new_scores=[]
+        for x in solutions:
+            # Contar una nueva evaluación.
+            n_eval+=1
+
+            # Evaluar diseño con diferentes accuracys para N.
+            for accuracy in list_acc:
+
+                # Actualizar el parámetro N.
+                N=int(default_N*accuracy)
+
+                # Transformación de parámetros.
+                turb_params=transform_turb_params(x, blade_number,bounds)
+
+                # Calcular score.
+                new_score=fitness_function(turb_params, N)
+
+                if N==default_N:
+                    new_scores.append(new_score)
+
+                # Añadir nuevos datos a la base de datos.
+                df.append([N,seed,n_gen,n_eval,-new_score,sw_eval_time.get_time()])
+
+        # Pasar los valores de la función objetivo para prepararse para la próxima iteración.
+        es.tell(solutions, new_scores)
+        es.logger.add()  
+
+        # Imprimir las variables del estado actual en una sola línea.
+        es.disp()
+
+      
+def new_stop(self, check=True, ignore_list=(), check_in_same_iteration=False,
+             get_value=None):
+	stop={}
+	if sw_stop.get_time()>max_time:
+		stop={'TIME RUN OUT':max_time}
+	return stop
+
+
+#==================================================================================================
+# PROGRAMA PRINCIPAL
+#==================================================================================================
+
+# Para usar la nueva función de parada (según el tiempo de ejecución).
+cma.CMAEvolutionStrategy.stop=new_stop
+
+# Definir array de rangos de los parámetros a optimizar (blase_number de forma individual).
+bounds=define_bounds()
+list_blade_number = [3, 5, 7]# Blade-number gene.
+
+# Mallados y parámetros.
+list_seeds=range(0,5,1)
+list_acc=[1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+max_time=60*9
+default_N=50
+
+#--------------------------------------------------------------------------------------------------
+# BLADE-NUMBER=3
+#--------------------------------------------------------------------------------------------------
+# Fijar blade-number.
+blade_number = 3
+
+# Inicializar base de datos.
+df=[]
+
+# Guardar datos asociados a cada semilla en una base de datos.	
+for seed in list_seeds:
+    # Evaluación.
+    evaluate(blade_number,bounds,seed)
+
+    # Guardar datos acumulados.
+    df=pd.DataFrame(df,columns=['N','seed','n_gen','n_eval','score','time'])
+    df.to_csv('results/data/Turbines/CMA_ES_GenerationAnalysis/df_blade_number'+str(blade_number)+'_seed'+str(seed)+'.csv')
+
+# Guardar lista de semillas.
+np.save('results/data/Turbines/CMA_ES_GenerationAnalysis/list_seeds',list_seeds)
+
+		
+		
+		
+
+
+
+
+
---
+++ experimentScripts_Turbines/CMA_ES_ConstantAccuracy_GenerationAnalysis_figures.py
@@ -0,0 +1,264 @@
+#==================================================================================================
+# LIBRERÍAS
+#==================================================================================================
+import numpy as np
+import matplotlib as mpl
+import scipy as sc
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import matplotlib.cm as cm
+from matplotlib.colors import ListedColormap
+import math
+
+#==================================================================================================
+# FUNCIONES
+#==================================================================================================
+
+def bootstrap_median_and_confiance_interval(data,bootstrap_iterations=1000):
+    mean_list=[]
+    for i in range(bootstrap_iterations):
+        sample = np.random.choice(data, len(data), replace=True) 
+        mean_list.append(np.mean(sample))
+    return np.mean(data),np.quantile(mean_list, 0.05),np.quantile(mean_list, 0.95)
+
+def common_n_gen_per_seed(blade_number,list_seeds):
+    n_gen_per_seed=math.inf
+
+    for seed in list_seeds:
+        df=pd.read_csv('results/data/Turbines/CMA_ES_GenerationAnalysis/df_blade_number'+str(blade_number)+'_seed'+str(seed)+'.csv',index_col=0)
+        max_n_gen=int(df['n_gen'].max())
+
+        if max_n_gen<n_gen_per_seed:
+            n_gen_per_seed=max_n_gen
+
+    return n_gen_per_seed
+
+def from_argsort_to_ranking(list):
+    new_list=[0]*len(list)
+    i=0
+    for j in list:
+        new_list[j]=i
+        i+=1
+    return new_list
+
+def ranking_matrix(df,gen):
+
+    # Reducir df a filas de interés.
+    df=df[df['n_gen']==gen]
+
+    # Guardar rankings de cada N en una matriz por filas.
+    matrix=[]
+    list_N=list(set(df['N']))
+    list_N.sort(reverse=True)
+ 
+    for N in list_N:
+        df_N=df[df['N']==N]
+        list_scores=df_N.sort_values('n_eval')['score']
+        ranking=from_argsort_to_ranking(np.argsort(-np.array(list_scores)))# Orden descendente.
+        matrix.append(ranking)
+
+    # Reordenar filas de la matriz para que la fila de la máxima precisión represente un ranking 
+    # ordenado de forma perfecta (de mejor a peor).
+    matrix_sort=[]
+    ind_sort=np.argsort(matrix[0])
+    for row in range(len(matrix)):
+
+        new_row=[]
+        for i in ind_sort:
+            new_row.append(matrix[row][i])
+
+        matrix_sort.append(new_row)
+    return matrix_sort
+
+
+def draw_rankings_per_generation(size,blade_number,seed):
+
+    # Cargar base de datos.
+    df=pd.read_csv('results/data/Turbines/CMA_ES_GenerationAnalysis/df_blade_number'+str(blade_number)+'_seed'+str(seed)+'.csv',index_col=0)
+
+    # Número de evaluaciones total.
+    total_n_eval=df['n_eval'].max()
+
+    # Valores de N considerados.
+    list_N=list(set(df['N']))
+    list_N.sort(reverse=True)
+
+    # Por cada generación dibujar la matriz de rankings.
+    for gen in range(1,size[1]+1):
+        ax=plt.subplot(size[0],size[1],gen+seed*size[1])
+
+        # Crear matriz de rankings.
+        matrix=np.matrix(ranking_matrix(df,gen))
+
+        # Dibujar la matriz
+        # color = sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95, reverse=True, as_cmap=True)
+        # color=cm.get_cmap(color)
+        # color=color(np.linspace(0,1,total_n_eval))
+        # color[:1, :]=np.array([248/256, 67/256, 24/256, 1])# Rojo (código rgb)
+        # color = ListedColormap(color)
+
+        color = sns.color_palette("deep",total_n_eval )
+
+        ax = sns.heatmap(matrix, cmap=color,linewidths=.5, linecolor='lightgray',cbar=False)
+        plt.xticks([])
+
+        if seed==size[0]-1:
+            ax.set_xlabel('Generation '+str(gen))
+
+        if gen ==1:
+            ax.set_ylabel('Seed '+str(seed)+'\n N')
+            ax.set_yticks(np.arange(.5,matrix.shape[0]+.5))
+            ax.set_yticklabels(list_N, rotation=0)
+
+        if gen !=1:
+            plt.yticks([])
+    return color
+    
+def pearson_corr(x,y):
+    return sc.stats.pearsonr(x,y)[0]
+
+def join_df(blade_number,list_seeds):
+
+    # Primera base de datos.
+    df=pd.read_csv('results/data/Turbines/CMA_ES_GenerationAnalysis/df_blade_number'+str(blade_number)+'_seed'+str(list_seeds[0])+'.csv',index_col=0)
+    
+    # Las demás.
+    for seed in list_seeds[1:]:
+        new_df=pd.read_csv('results/data/Turbines/CMA_ES_GenerationAnalysis/df_blade_number'+str(blade_number)+'_seed'+str(list_seeds[seed])+'.csv',index_col=0)
+        df=pd.concat([df,new_df])    
+    return df
+
+def score_matrix(df_seed,gen):
+
+     # Reducir df a filas de interés.
+    df_seed_gen=df_seed[df_seed['n_gen']==gen]
+
+    # Guardar scores de cada N en una matriz por filas.
+    matrix=[]
+    list_N=list(set(df_seed_gen['N']))
+    list_N.sort(reverse=True)
+
+    for N in list_N:
+        df_N=df_seed_gen[df_seed_gen['N']==N]
+        list_scores=df_N.sort_values('n_eval')['score']
+        matrix.append(list(list_scores))
+
+    return matrix
+
+
+def draw_ranking_comparison_per_gen(blade_number,list_seeds,type,what):
+
+    # Juntar todas las bases de datos en una.
+    df=join_df(blade_number,list_seeds)
+
+    # Valores de N considerados.
+    list_N=list(set(df['N']))
+    list_N.sort(reverse=True)
+
+    # Número de generaciones a considerar.
+    n_gen_considered=min(df.groupby('seed')['n_gen'].max())
+
+    # Inicializar gráfica.
+    ax=plt.subplot(111)
+
+    # Para cada valor de N dibujar una curva.
+    for ind_N in range(len(list_N)):
+        # Inicializar listas.
+        all_mean=[]
+        all_q05=[]
+        all_q95=[]
+
+        # Por generación ir actualizando las listas anteriores.
+        for gen in range(1,n_gen_considered+1):
+            list_metric=[]
+            for seed in list_seeds:
+                df_seed=df[df['seed']==seed]
+                if what=='scores':
+                    matrix=score_matrix(df_seed,gen)
+                if what=='positions':
+                    matrix=ranking_matrix(df_seed,gen)
+                if type=='pearson':
+                    list_metric.append(pearson_corr(matrix[0],matrix[ind_N]))
+
+
+            mean,q05,q95=bootstrap_median_and_confiance_interval(list_metric)
+            all_mean.append(mean)
+            all_q05.append(q05)
+            all_q95.append(q95)
+        
+        # Dibujar curva.
+        ax.fill_between(range(1,n_gen_considered+1),all_q05,all_q95,alpha=.5,linewidth=0)
+        plt.plot(range(1,n_gen_considered+1),all_mean,linewidth=2,label=str(list_N[ind_N]))
+
+    # Detalles de la gráfica.
+    ax.set_ylabel('Pearson correlation')
+    ax.set_xlabel('Generation')
+    ax.set_title('Comparing similarity between the perfect \n and each '+str(what)+' ranking (blade-number '+str(blade_number)+')')
+    ax.legend(title="N",bbox_to_anchor=(1.2, 0, 0, 1), loc='center')
+
+
+#==================================================================================================
+# PROGRAMA PRINCIPAL
+#==================================================================================================
+#--------------------------------------------------------------------------------------------------
+# Gráfica 1: rankings para blade-number 3
+#--------------------------------------------------------------------------------------------------
+# Cargar datos.
+list_seeds=np.load('results/data/Turbines/CMA_ES_GenerationAnalysis/list_seeds.npy')
+
+# Blade-number.
+blade_number=3
+
+# Número de generaciones en común para todas las semillas.
+n_gen_per_seed=common_n_gen_per_seed(blade_number,list_seeds)
+
+# Inicializar estructura de la gráfica.
+plt.figure(figsize=[15,9])
+plt.subplots_adjust(left=0.1,bottom=0.05,right=0.95,top=0.93,wspace=0.14,hspace=0.3)
+
+for seed in list_seeds:
+    color=draw_rankings_per_generation([len(list_seeds),n_gen_per_seed],blade_number,seed)
+
+# Barra de colores.
+# ax = plt.subplot(len(list_seeds)+1,n_gen_per_seed,(len(list_seeds)*n_gen_per_seed+2,(len(list_seeds)+1)*n_gen_per_seed-1))
+# mpl.colorbar.ColorbarBase(ax, cmap=color,orientation='horizontal')
+# ax.set_xlabel('Ranking position')
+# ax.set_xticks(np.arange(.05,1.05,0.1))
+# ax.set_xticklabels(range(1,10+1,1))
+
+
+plt.figtext(0.5, 0.95, 'Rankings per generation (blade-number '+str(blade_number)+')', ha='center', va='center')
+plt.savefig('results/figures/Turbines/CMA_ES_GenerationAnalysis_rankings.png')
+plt.show()
+plt.close()
+
+#--------------------------------------------------------------------------------------------------
+# Gráfica 2: comparación de rankings (usando los scores) por generación blade-number 3
+#--------------------------------------------------------------------------------------------------
+# Inicializar gráfica.
+plt.figure(figsize=[7,5])
+plt.subplots_adjust(left=0.13,bottom=0.11,right=0.77,top=0.87,wspace=0.14,hspace=0.3)
+
+
+# Crear y guardar gráfica.
+draw_ranking_comparison_per_gen(blade_number,list_seeds,'pearson','scores')
+plt.savefig('results/figures/Turbines/CMA_ES_GenerationAnalysis_comparison_scores.png')
+plt.show()
+plt.close()
+
+#--------------------------------------------------------------------------------------------------
+# Gráfica 3: comparación de rankings (usando los posiciones) por generación blade-number 3
+#--------------------------------------------------------------------------------------------------
+# Inicializar gráfica.
+plt.figure(figsize=[7,5])
+plt.subplots_adjust(left=0.13,bottom=0.11,right=0.77,top=0.87,wspace=0.14,hspace=0.3)
+
+
+# Crear y guardar gráfica.
+draw_ranking_comparison_per_gen(blade_number,list_seeds,'pearson','positions')
+plt.savefig('results/figures/Turbines/CMA_ES_GenerationAnalysis_comparison_positions.png')
+plt.show()
+plt.close()
+
+
