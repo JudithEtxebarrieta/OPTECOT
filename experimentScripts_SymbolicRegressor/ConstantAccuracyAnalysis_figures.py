f9631d3a02a1a21e1aa32130ef76ce1da4533620
JudithEtxebarrieta <judithetxebarrieta@gmail.com>
2022-12-22 10:13:36 +0100

    change figure

--- experimentScripts_SymbolicRegressor/ConstantAccuracyAnalysis_figures.py
+++ experimentScripts_SymbolicRegressor/ConstantAccuracyAnalysis_figures.py
@@ -68,28 +68,7 @@ def train_data_to_figure_data(df_train_acc,list_train_n_eval):
         all_q95.append(q95)
 
     return all_mean,all_q05,all_q95
-
-# FUNCIÓN 3
-# Parámetros:
-#   >position: código numérico con la posición donde se desea dibujar la gráfica de los errores
-#    absolutos medios.
-#   >eval_expr: expresión de la superficie que se desea dibujar.
-# Devolver: nada, se dibuja directamente la gráfica.
-
-def draw_surface(position,eval_expr):
-
-    # Calcular coordenadas de los puntos que se desean dibujar.
-	x = np.arange(-1, 1, 1/10.)
-	y = np.arange(-1, 1, 1/10.)
-	x, y= np.meshgrid(x, y)
-	z = eval(eval_expr)
-
-    # Dibujar todos los puntos.
-	ax = plt.subplot(position,projection='3d')
-	ax.set_xlim(-1, 1)
-	ax.set_ylim(-1, 1)
-	ax.plot_surface(x, y, z, rstride=1, cstride=1,color='green', alpha=0.5)
-	ax.set_title('Real surface: '+eval_expr)  
+  
 #==================================================================================================
 # PROGRAMA PRINCIPAL
 #==================================================================================================
@@ -98,17 +77,20 @@ def draw_surface(position,eval_expr):
 # GRÁFICA 1 (Resultados generales scores)
 #--------------------------------------------------------------------------------------------------
 print('GRAFICA 1')
-plt.figure(figsize=[18,6])
-plt.subplots_adjust(left=0.02,bottom=0.11,right=0.89,top=0.88,wspace=0.36,hspace=0.2)
+plt.figure(figsize=[8,6])
+plt.subplots_adjust(left=0.12,bottom=0.11,right=0.74,top=0.88,wspace=0.2,hspace=0.2)
 
 # Leer lista con valores de accuracy considerados.
 list_acc=np.load('results/data/SymbolicRegressor/list_acc.npy')
+
+# Leer expresión de superficie real.
+eval_expr=str(np.load('results/data/SymbolicRegressor/expr_surf.npy'))
 
 # Lista con límites de número de evaluaciones de entrenamiento que se desean dibujar.
 list_train_n_eval=range(100000,10100000,100000)
 
 # Ir dibujando una curva por cada valor de accuracy.
-ax=plt.subplot(133)
+ax=plt.subplot(111)
 for accuracy in list_acc:
 
     # Leer base de datos.
@@ -124,31 +106,10 @@ for accuracy in list_acc:
 
 ax.set_xlabel("Train evaluations")
 ax.set_ylabel("Mean score (MAE)")
-ax.set_title('Model evaluation (train 30 seeds, test 50 pts)')
+ax.set_title('Model evaluation \n (train 30 seeds, test 50 pts, real surface '+str(eval_expr)+')')
 ax.legend(title="Train set point \n size accuracy",bbox_to_anchor=(1.2, 0, 0, 1), loc='center')
+plt.xscale('log')
 plt.yscale('log')
-
-#Zoom
-axzoom = plt.subplot(132)
-for accuracy in list_acc:
-
-    # Leer base de datos.
-    df_train_acc=pd.read_csv("results/data/SymbolicRegressor/df_train_acc"+str(accuracy)+".csv", index_col=0)
-
-    # Extraer de la base de datos la información relevante.
-    all_mean_scores,all_q05_scores,all_q95_scores =train_data_to_figure_data(df_train_acc,list_train_n_eval)
-
-    # Dibujar curva.
-    axzoom.fill_between(list_train_n_eval,all_q05_scores,all_q95_scores, alpha=.5, linewidth=0)
-    plt.plot(list_train_n_eval, all_mean_scores, linewidth=2,label=str(accuracy))
-
-axzoom.set_xlim(0,600000)
-axzoom.set_ylim(0.05,0.37)
-axzoom.set_title('ZOOM')
-
-# Superficie.
-eval_expr=str(np.load('results/data/SymbolicRegressor/expr_surf.npy'))
-draw_surface(131,eval_expr)
 plt.savefig('results/figures/SymbolicRegressor/general_results.png')
 plt.show()
 plt.close()
@@ -195,7 +156,7 @@ ax=plt.subplot(111)
 ax.bar(train_times_sort,max_scores_sort,acc_sort,label=acc_sort_str,color=colors)
 ax.set_xlabel("Train evaluations")
 ax.set_ylabel("Score (MAE)")
-ax.set_title('Best results for each model')
+ax.set_title('Best results for each model \n (real surface '+str(eval_expr)+')')
 ax.legend(title="Train set point \n size accuracy",bbox_to_anchor=(1.2, 0, 0, 1), loc='center')
 plt.axhline(y=score_limit,color='red', linestyle='--')
 plt.savefig('results/figures/SymbolicRegressor/best_results.png')
