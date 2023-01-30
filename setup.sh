78f780a29bd48a3f14558328c692483942d3ed78
EtorArza <etorarza@gmail.com>
2023-01-30 12:28:41 +0100

    working mujoco with custom accuracy

---
+++ setup.sh
@@ -0,0 +1,9 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -U pip
+
+pip install -r requirements.txt
+pip install -r cleanrl-master/requirements/requirements-mujoco.txt
+pip install -r cleanrl-master/requirements/requirements-mujoco_py.txt
+
+
