5d25fd095a6903c4894aea606194b9b88d4c1e3a
JudithEtxebarrieta <judithetxebarrieta@gmail.com>
2023-02-27 09:15:20 +0100

    update MuJoCo setup

--- others/setup.sh
+++ others/setup.sh
@@ -24,16 +24,32 @@ pip install plotly
 pip install openpyxl
 pip install termcolor
 
-# MuJoCo
-sudo apt-get install python3-venv
-python3 -m venv venv
+# MuJoCo 
+sudo apt-get install python3.8-venv
+python3.8 -m venv venv
 source venv/bin/activate
 pip install -U pip
 pip install swig
 
-pip install -r requirements.txt
+pip install -r others/requirements.txt
 pip install -r cleanrl/requirements/requirements-mujoco.txt
 pip install -r cleanrl/requirements/requirements-mujoco_py.txt
+
+cd ~/Downloads
+wget -nc -c  wget -c 	https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz  -O mujoco210.tar.gz
+tar -x -k -f mujoco210.tar.gz
+mkdir -p ~/.mujoco
+rsync -vraR mujoco210 ~/.mujoco/
+
+pip3 install 'mujoco-py<2.2,>=2.1'
+
+pip install git+https://github.com/rlworkgroup/metaworld.git@master#egg=metaworld
+
+pip install TensorFlow==2.11
+pip install torch>=1.11
+pip install garage
+pip install pyglet
+pip install tensorflow-cpu
 
 # WindFLO
 pip install f90nml
