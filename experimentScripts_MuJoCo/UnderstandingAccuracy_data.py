1cd027038591fb88285bb1812bb0cbed273d61af
JudithEtxebarrieta <judithetxebarrieta@gmail.com>
2023-03-09 14:35:44 +0100

    add new experiment and its results

---
+++ experimentScripts_MuJoCo/UnderstandingAccuracy_data.py
@@ -0,0 +1,192 @@
+# Mediante este script se evalúan 100 políticas aleatorias (asociadas a una precisión máxima) 
+# sobre 10 episodios de un entorno definido con 10 valores de accuracy diferentes para el parámetro
+# timesteps (tiempo transcurrido entre frame y frame de un episodio). Los datos relevantes (medias 
+# de rewards y número de steps por evaluación) se almacenan para después poder acceder a ellos.
+
+#==================================================================================================
+# LIBRERÍAS
+#==================================================================================================
+import os
+import sys
+from shutil import rmtree
+os.environ['TF_ENABLE_ONEDNN_OPTS']='0'
+
+import numpy as np
+from garage.envs import GymEnv
+import pandas as pd
+from garage.tf.policies import CategoricalMLPPolicy,ContinuousMLPPolicy
+from garage.tf.policies.policy import Policy
+from garage import wrap_experiment
+from garage.trainer import TFTrainer
+from tqdm import tqdm as tqdm
+from garage.experiment.deterministic import set_seed
+from garage.np.algos import CMAES
+from garage.sampler import LocalSampler
+import garage.sampler.default_worker
+from garage.sampler.default_worker import *
+import dowel.logger
+from dowel.logger import *
+
+import tensorflow as tf
+from tensorflow.python.ops.variable_scope import VariableScope
+VariableScope.reuse=tf.compat.v1.AUTO_REUSE
+
+import multiprocessing as mp
+import psutil as ps
+
+
+#==================================================================================================
+# FUNCIONES
+#==================================================================================================
+# FUNCIÓN 1 (Esta función es una modificación de otra ya existente, con la modificación se pretenden
+# ir almacenando los datos de interés).
+def rollout(self):
+
+    self.start_episode()
+    while not self.step_episode():
+        pass
+
+    # MODIFICACIÓN: cada vez que se entre en esta función estaremos trabajando con una política nueva,
+    # por tanto es el momento en donde debemos evaluar la política y guardar la información de interés.
+    global n_policy,global_n_sample
+    print('Policy: '+str(n_policy)+str('/')+str(global_n_sample))
+    for accuracy in tqdm(list_acc):
+        reward,steps=evaluate_policy(self.agent,accuracy)
+        df.append([accuracy,n_policy,reward,steps])
+    n_policy+=1
+
+    return self.collect_episode()
+
+# FUNCIÓN 2 (evaluar una muestra aleatoria de políticas, para ello se considerarán los 
+# individuos de una única generación del algoritmo CMA-ES)
+@wrap_experiment
+def evaluate_policy_sample(ctxt=None,n_sample=100):
+
+    global global_n_sample
+    global_n_sample=n_sample
+
+    with TFTrainer(ctxt) as trainer:
+
+        set_seed(0)
+
+        # Inicializar entorno sobre el cual se definirán las políticas (con máximo accuracy).
+        env = GymEnv(gymEnvName, max_episode_length=max_episode_length)
+        env._env.seed(seed=0)
+
+        # En caso de usar un entorno con criterio de parada dependiente del parámetro 
+        # terminate_when_unhealthy, anular este tipo de parada.
+        if DTU:
+            env._env.env._terminate_when_unhealthy = False 
+
+        # Evaluar muestra aleatoria de 100 políticas con los accuracys seleccionados.
+        global n_policy
+        n_policy=1
+        is_action_space_discrete=bool(["continuous", "discrete"].index(action_space))
+        if is_action_space_discrete:
+            policy = CategoricalMLPPolicy(name=policy_name, env_spec=env.spec)
+        else:
+            policy = ContinuousMLPPolicy(name=policy_name, env_spec=env.spec)
+
+        sampler = LocalSampler(agents=policy, envs=env, max_episode_length=env.spec.max_episode_length, is_tf_worker=True)
+        algo= CMAES(env_spec=env.spec, policy=policy, sampler=sampler, n_samples=n_sample)
+        trainer.setup(algo, env)
+        trainer.train(n_epochs=1, batch_size=env.spec.max_episode_length) # Esta función llamará internamente a "rollout".
+
+# FUNCIÓN 3 (evaluar una política)
+def evaluate_policy(policy,accuracy):
+
+    # Inicializar entorno de validación fijando el valor de accuracy adecuado.
+    eval_env = GymEnv(gymEnvName, max_episode_length=max_episode_length*accuracy)
+    eval_env._env.unwrapped.model.opt.timestep=default_frametime/accuracy
+
+    # Fijar semilla para que los episodios a evaluar sean los mismos por cada llamada a la función 
+    # y definir el estado inicial (obs) del primer episodio.
+    eval_env._env.seed(seed=0)
+    obs,_=eval_env.reset()
+
+    # Guardar reward por episodios evaluado.
+    all_ep_reward=[]
+    steps=0
+    for _ in range(10):
+        # Evaluar episodio con la política y guardar el reward asociado.
+        episode_reward=0
+        done=False
+        while not done:
+            action,_=policy.get_action(obs)
+            env_step_info=eval_env.step(action)
+            episode_reward+=env_step_info.reward
+            done=env_step_info.last
+            obs=env_step_info.observation
+            steps+=1
+        all_ep_reward.append(episode_reward)
+        obs,_=eval_env.reset()
+
+    reward=np.mean(all_ep_reward)
+
+    return reward,steps
+ 
+#==================================================================================================
+# PROGRAMA PRINCIPAL
+#==================================================================================================
+# Modificación de función existente (para guardar datos durante el entrenamiento).
+garage.sampler.default_worker.DefaultWorker.rollout = rollout
+# Modificación de función existente ( para no perder tiempo).
+garage.trainer.Trainer.save = lambda self, epoch: "skipp save."
+# Modificación de función existente (para no imprimir información de episodio durante el entrenamiento).
+Logger.log= lambda self, data: 'skipp info message.'
+warnings.filterwarnings("ignore")
+
+# Características del entorno (para ejecutar el script con otro entorno MuJoCo estas son las únicas 
+# variables que habrá que modificar).
+gymEnvName='Swimmer-v3'
+action_space="continuous"
+max_episode_length=1000
+default_frametime=0.01 # Parámetro del que se modificará el accuracy.
+policy_name='SwimmerPolicy'
+DTU=False # Criterio de parada dependiente de terminate_when_unhealthy.
+
+#--------------------------------------------------------------------------------------------------
+# Para el análisis de motivación.
+#--------------------------------------------------------------------------------------------------
+
+# Lista de accuracys con los que se evaluará la muestra anterior.
+list_acc=[1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
+
+# Evaluar una muestra aleatoria de 100 políticas usando diferentes valores de accuracy y construir una 
+# base de datos con los scores (rewards) y tiempos de ejecución (steps) por evaluación.
+df=[]
+evaluate_policy_sample(n_sample=100)
+
+# Guardar base de datos.
+df=pd.DataFrame(df,columns=['accuracy','n_policy','reward','steps'])
+df.to_csv('results/data/MuJoCo/UnderstandingAccuracy/df_UnderstandingAccuracy.csv')
+
+#--------------------------------------------------------------------------------------------------
+# Para la fijación del umbral del método de bisección.
+#--------------------------------------------------------------------------------------------------
+# Lista con los valores de accuracy que se considerarían por el método de bisección, teniendo en
+# cuenta que el criterio de parada es alcanzar un rango del intervalo de 0.1 y suponiendo que
+# en todas las iteraciones se acota el intervalo por arriba (caso más costoso).
+def upper_middle_point(lower,upper=1.0):
+    list=[] 
+    while abs(lower-upper)>0.1:       
+        middle=(lower+upper)/2
+        list.append(middle)
+        lower=middle
+    return list
+
+list_acc=upper_middle_point(1/max_episode_length)+[1.0]
+
+# Evaluar una muestra aleatoria usando los valores anteriores de accuracy.
+df=[]
+evaluate_policy_sample(n_sample=100)
+
+df_bisection=pd.DataFrame(df,columns=['accuracy','n_policy','reward','cost_per_eval'])
+df_bisection=df_bisection[['accuracy','cost_per_eval']]
+df_bisection=df_bisection.groupby('accuracy').mean()
+df_bisection.to_csv('results/data/MuJoCo/UnderstandingAccuracy/df_BisectionThreshold.csv')
+
+# Eliminar ficheros auxiliares.
+sys.path.append('data')
+rmtree('data')
+
---
+++ experimentScripts_MuJoCo/UnderstandingAccuracy_figures.py
@@ -0,0 +1,364 @@
+# Mediante este script se representan gráficamente los resultados numéricos calculados por 
+# "UnderstandingAccuracy_data.py".
+
+#==================================================================================================
+# LIBRERÍAS
+#==================================================================================================
+import numpy as np
+from scipy.stats import norm
+import time
+from tqdm import tqdm as tqdm
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import random
+import pandas as pd
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes,mark_inset
+import plotly.express as px
+import seaborn as sns
+import matplotlib.cm as cm
+from matplotlib.colors import ListedColormap
+import scipy as sc
+from itertools import combinations
+
+#==================================================================================================
+# FUNCIONES
+#==================================================================================================
+# FUNCIÓN 1
+# Parámetros:
+#   >data: datos sobre los cuales se calculará el rango entre percentiles.
+#   >bootstrap_iterations: número de submuestras que se considerarán de data para poder calcular el 
+#    rango entre percentiles de sus medias.
+# Devolver: la media de los datos originales junto a los percentiles de las medias obtenidas del 
+# submuestreo realizado sobre data.
+
+def bootstrap_mean_and_confidence_interval(data,bootstrap_iterations=1000):
+    mean_list=[]
+    for i in range(bootstrap_iterations):
+        sample = np.random.choice(data, len(data), replace=True) 
+        mean_list.append(np.mean(sample))
+    return np.mean(data),np.quantile(mean_list, 0.05),np.quantile(mean_list, 0.95)
+
+# FUNCIÓN 2 (Obtener ranking a partir de lista conseguida tras aplicar "np.argsort" sobre una lista original)
+def from_argsort_to_ranking(list):
+    new_list=[0]*len(list)
+    i=0
+    for j in list:
+        new_list[j]=i
+        i+=1
+    return new_list
+
+# FUNCIÓN 3 (Ordenar lista según el orden para cada posición definido en argsort)
+def custom_sort(list,argsort):
+    new_list=[]
+    for index in argsort:
+        new_list.append(list[index])
+    return new_list
+
+
+# FUNCIÓN 4 (Cálculo de la distancia inversa normalizada de tau kendall entre dos rankings)
+def inverse_normalized_tau_kendall(x,y):
+    # Número de pares con orden inverso.
+    pairs_reverse_order=0
+    for i in range(len(x)):
+        for j in range(i+1,len(x)):
+            case1 = x[i] < x[j] and y[i] > y[j]
+            case2 = x[i] > x[j] and y[i] < y[j]
+
+            if case1 or case2:
+                pairs_reverse_order+=1  
+    
+    # Número de pares total.
+    total_pairs=len(list(combinations(x,2)))
+    # Distancia tau Kendall normalizada.
+    tau_kendall=pairs_reverse_order/total_pairs
+
+    return 1-tau_kendall
+
+# FUNCIÓN 5 (Eliminar elementos en ciertas posiciones de una lista)
+def remove_element_in_idx(list_elements,list_idx):
+    new_list=[]
+    for i in range(len(list_elements)):
+        if i not in list_idx:
+            new_list.append(list_elements[i])
+    return new_list
+
+# FUNCIÓN 6 (Construir las gráficas deseadas a partir de la base de datos)
+def from_data_to_figures(df):
+
+    # Inicializar gráfica.
+    plt.figure(figsize=[15,10])
+    plt.subplots_adjust(left=0.06,bottom=0.11,right=0.95,top=0.88,wspace=0.3,hspace=0.69)
+
+    #----------------------------------------------------------------------------------------------
+    # GRÁFICA 1: tiempo de ejecución por accuracy.
+    #----------------------------------------------------------------------------------------------
+    ax=plt.subplot2grid((4, 4), (0,0), colspan=2,rowspan=2)
+    # ax=plt.subplot(221)
+
+    list_acc=list(set(df['accuracy']))
+    list_acc.sort()
+
+    all_means=[]
+    all_q05=[]
+    all_q95=[]
+
+    for accuracy in list_acc:
+        mean,q05,q95=bootstrap_mean_and_confidence_interval(list(df[df['accuracy']==accuracy]['steps']))
+        all_means.append(mean)
+        all_q05.append(q05)
+        all_q95.append(q95)
+
+    ax.fill_between(list_acc,all_q05,all_q95, alpha=.5, linewidth=0)
+    plt.plot(list_acc, all_means, linewidth=2) 
+    ax.set_xlabel("Accuracy")
+    ax.set_ylabel("Steps per evaluation")
+    ax.set_title('Evaluation cost depending on accuracy')
+
+
+    #----------------------------------------------------------------------------------------------
+    # GRÁFICA 2: extra de evaluaciones.
+    #----------------------------------------------------------------------------------------------
+    ax=plt.subplot2grid((4, 4), (0,2), colspan=2,rowspan=2)
+    # ax=plt.subplot(222)
+
+    list_total_times=list(df.groupby('accuracy')['steps'].sum())
+
+    list_extra_eval=[]
+    for i in range(0,len(list_total_times)):
+        # Ahorro de tiempo.
+        save_time=list_total_times[-1]-list_total_times[i]
+        # Número de evaluaciones extra que se podrian hacer para agotar el tiempo que se necesita por defecto.
+        extra_eval=int(save_time/all_means[i])
+        list_extra_eval.append(extra_eval)
+
+    plt.bar([str(acc) for acc in list_acc],list_extra_eval)
+    ax.set_xlabel('Accuracy')
+    ax.set_ylabel('Number of extra evaluations')
+    plt.xticks(rotation = 0)
+    ax.set_title('Extra evaluations in the same amount of time required for maximum accuracy')
+
+
+    #----------------------------------------------------------------------------------------------
+    # GRÁFICA 3: comparación de ranking asociados a las 100 soluciones con cada accuracy.
+    #----------------------------------------------------------------------------------------------
+    def ranking_matrix_sorted_by_max_acc(ranking_matrix):
+        # Argumentos asociados al ranking del accuracy 1 ordenado de menor a mayor.
+        argsort_max_acc=np.argsort(ranking_matrix[-1])
+
+        # Reordenar filas de la matriz.
+        sorted_ranking_list=[]
+        for i in range(len(ranking_list)):
+            sorted_ranking_list.append(custom_sort(ranking_list[i],argsort_max_acc))
+
+        return sorted_ranking_list
+
+    ax=plt.subplot(223)
+
+    ranking_list=[]
+
+    for accuracy in list_acc:
+        df_acc=df[df['accuracy']==accuracy]
+        ranking=from_argsort_to_ranking(list(df_acc['reward'].argsort()))
+        ranking_list.append(ranking)
+    
+    ranking_matrix=np.matrix(ranking_matrix_sorted_by_max_acc(ranking_list))
+
+    color = sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95, reverse=True, as_cmap=True)
+    color=cm.get_cmap(color)
+    color=color(np.linspace(0,1,ranking_matrix.shape[1]))
+    color[:1, :]=np.array([248/256, 67/256, 24/256, 1])# Rojo (código rgb)
+    color = ListedColormap(color)
+
+    ax = sns.heatmap(ranking_matrix, cmap=color,linewidths=.5, linecolor='lightgray')
+
+    colorbar=ax.collections[0].colorbar
+    colorbar.set_label('Ranking position')
+    colorbar.set_ticks(range(0,ranking_matrix.shape[1],10))
+    colorbar.set_ticklabels(range(1,ranking_matrix.shape[1]+1,10))
+
+    ax.set_xlabel('Solution')
+    ax.set_xticks(range(0,ranking_matrix.shape[1],10))
+    ax.set_xticklabels(range(1,ranking_matrix.shape[1]+1,10),rotation=0)
+
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Comparing rankings depending on accuracy')
+    ax.set_yticks(np.arange(0.5,len(list_acc)+0.5,1))
+    ax.set_yticklabels(list_acc,rotation=0)
+
+    #----------------------------------------------------------------------------------------------
+    # GRÁFICA 4: perdida de calidad.
+    #----------------------------------------------------------------------------------------------
+    ax=plt.subplot2grid((4, 4), (2,2), colspan=2,rowspan=2)
+    # ax=plt.subplot(224)
+
+    # Similitud entre rankings enteros.
+    list_corr=[]
+    for i in range(ranking_matrix.shape[0]):
+        list_corr.append(inverse_normalized_tau_kendall(ranking_list[-1],ranking_list[i]))
+
+    plt.plot(list_acc, list_corr, linewidth=2,label='All ranking') 
+
+    # Similitud entre el 50% inicial de los rankings.
+    list_ind=[]
+    for i in range(int(ranking_matrix.shape[1]*0.5),ranking_matrix.shape[1]):
+        ind=ranking_list[-1].index(i)
+        list_ind.append(ind)
+
+    list_corr=[]
+    for i in range(ranking_matrix.shape[0]):
+        best_ranking=remove_element_in_idx(ranking_list[-1],list_ind)
+        lower_ranking=remove_element_in_idx(ranking_list[i],list_ind)
+        list_corr.append(inverse_normalized_tau_kendall(best_ranking,lower_ranking))
+
+    plt.plot(list_acc, list_corr, linewidth=2,label='The best 50%') 
+
+    # Similitud entre el 10% inicial de los rankings.
+    list_ind=[]
+    for i in range(int(ranking_matrix.shape[1]*0.1),ranking_matrix.shape[1]):
+        ind=ranking_list[-1].index(i)
+        list_ind.append(ind)
+
+    list_corr=[]
+    for i in range(ranking_matrix.shape[0]):
+        best_ranking=remove_element_in_idx(ranking_list[-1],list_ind)
+        lower_ranking=remove_element_in_idx(ranking_list[i],list_ind)
+        list_corr.append(inverse_normalized_tau_kendall(best_ranking,lower_ranking))
+
+    plt.plot(list_acc, list_corr, linewidth=2,label='The best 10%') 
+    ax.legend(title="Similarity between")
+    ax.set_xlabel("Accuracy")
+    ax.set_ylabel("1 - normalized tau Kendall")
+    ax.set_title('Comparing the similarity between the best and the rest rankings')
+    # ax.set_xscale('log')
+
+    # Guardar imagen.
+    plt.savefig('results/figures/MuJoCo/UnderstandingAccuracy.png')
+    plt.show()
+    plt.close()
+
+# FUNCIÓN 7 (Gráficas para el paper)
+def from_data_to_figures_paper(df):
+
+    # Inicializar gráfica.
+    fig=plt.figure(figsize=[15,3.5],constrained_layout=True)
+    # plt.subplots_adjust(left=0.06,bottom=0.21,right=0.95,top=0.78,wspace=0.1,hspace=0.69)
+    subfigs = fig.subfigures(1, 4, width_ratios=[2,2,0.1,4], wspace=0)
+
+    #----------------------------------------------------------------------------------------------
+    # GRÁFICA 1: tiempo de ejecución por accuracy y extra de evaluaciones.
+    #----------------------------------------------------------------------------------------------
+    list_acc=list(set(df['accuracy']))
+    list_acc.sort()
+    list_acc_str=[str(acc) for acc in list_acc]
+    list_acc_str.reverse()
+
+    # Tiempos de ejecución por evaluación.
+    all_means=[]
+    all_q05=[]
+    all_q95=[]
+
+    for accuracy in list_acc:
+        mean,q05,q95=bootstrap_mean_and_confidence_interval(list(df[df['accuracy']==accuracy]['steps']))
+        all_means.append(mean)
+        all_q05.append(q05)
+        all_q95.append(q95)
+
+    # Evaluaciones extra.
+    list_total_times=list(df.groupby('accuracy')['steps'].sum())
+    list_extra_eval=[]
+    for i in range(0,len(list_total_times)):
+        # Ahorro de tiempo.
+        save_time=list_total_times[-1]-list_total_times[i]
+        # Número de evaluaciones extra que se podrían hacer para agotar el tiempo que se necesita por defecto.
+        extra_eval=int(save_time/all_means[i])
+        list_extra_eval.append(extra_eval)
+
+    color = sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95, reverse=True, as_cmap=True)
+    color=cm.get_cmap(color)
+    color=color(np.linspace(0,1,3))
+
+    ax=subfigs[0].subplots()
+    plt.barh(list_acc_str, [i*(-1) for i in all_means], align='center',color=color[0])
+    plt.title("\n Cost per evaluation")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Steps")
+    ax.set_xticks(range(0,-12000,-2000))
+    ax.set_xticklabels(range(0,12000,2000),rotation=0)
+
+    ax=subfigs[1].subplots()
+    for i in range(len(list_extra_eval)):
+        if list_extra_eval[i]<0:
+            list_extra_eval[i]=0
+    plt.barh(list_acc_str, list_extra_eval, align='center',color=color[1])
+    plt.yticks([])
+    plt.title("Extra evaluations in the same amount of \n time required for maximum accuracy")
+    # plt.ylabel("Accuracy")
+    plt.xlabel("Evaluations")
+
+
+
+    #----------------------------------------------------------------------------------------------
+    # GRÁFICA 2: perdida de calidad en las evaluaciones considerar accuracys menores y existencia 
+    # de un accuracy menor con el que se obtiene la máxima calidad.
+    #----------------------------------------------------------------------------------------------
+    def ranking_matrix_sorted_by_max_acc(ranking_matrix):
+        # Argumentos asociados al ranking del accuracy 1 ordenado de menor a mayor.
+        argsort_max_acc=np.argsort(ranking_matrix[-1])
+
+        # Reordenar filas de la matriz.
+        sorted_ranking_list=[]
+        for i in range(len(ranking_list)):
+            sorted_ranking_list.append(custom_sort(ranking_list[i],argsort_max_acc))
+
+        return sorted_ranking_list
+
+    def absolute_distance_matrix_between_rankings(ranking_matrix):
+        abs_dist_matrix=[]
+        for i in range(len(ranking_matrix)):
+            abs_dist_matrix.append(np.abs(list(np.array(ranking_matrix[i])-np.array(ranking_matrix[-1]))))
+
+        max_value=max(max(i) for i in abs_dist_matrix)
+        norm_abs_dist_matrix=list(np.array(abs_dist_matrix)/max_value)
+        
+        return norm_abs_dist_matrix
+
+    ax=subfigs[3].subplots()
+
+    ranking_list=[]
+
+    for accuracy in list_acc:
+        df_acc=df[df['accuracy']==accuracy]
+        ranking=from_argsort_to_ranking(list(df_acc['reward'].argsort()))
+        ranking_list.append(ranking)
+
+    ranking_matrix=np.matrix(absolute_distance_matrix_between_rankings(ranking_matrix_sorted_by_max_acc(ranking_list)))
+    color = sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95, reverse=False, as_cmap=True)
+
+    ax = sns.heatmap(ranking_matrix, cmap=color,linewidths=.5, linecolor='lightgray')
+
+    colorbar=ax.collections[0].colorbar
+    colorbar.set_label('Normalized distance')
+
+    ax.set_xlabel('Solution (sorted by the ranking of maximum accuracy)')
+    ax.set_xticks(range(0,ranking_matrix.shape[1],10))
+    ax.set_xticklabels(range(1,ranking_matrix.shape[1]+1,10),rotation=0)
+
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Normalized absolute distances between the respective positions \n of the best and the rest rankings')
+    ax.set_yticks(np.arange(0.5,len(list_acc)+0.5,1))
+    ax.set_yticklabels(list_acc,rotation=0)
+
+    # Guardar imagen.
+    plt.savefig('results/figures/MuJoCo/UnderstandingAccuracy_paper.png')
+    plt.show()
+    plt.close()
+
+#==================================================================================================
+# PROGRAMA PRINCIPAL
+#==================================================================================================
+# Leer base de datos.
+df=pd.read_csv('results/data/MuJoCo/UnderstandingAccuracy/df_UnderstandingAccuracy.csv',index_col=0)
+
+# Construir gráfica.
+from_data_to_figures(df)
+from_data_to_figures_paper(df)
