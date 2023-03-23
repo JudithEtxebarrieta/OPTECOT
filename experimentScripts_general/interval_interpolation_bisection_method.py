5cb4eeaee6ab7c94e5cd413b6f5663ce6aadd506
JudithEtxebarrieta <judithetxebarrieta@gmail.com>
2023-03-23 10:38:24 +0100

    add experiments (sample size, frequency and interpolation between evaluation times and accuracys to apply bisection method)

---
+++ experimentScripts_general/interval_interpolation_bisection_method.py
@@ -0,0 +1,57 @@
+
+
+#==================================================================================================
+# LIBRERÍAS
+#==================================================================================================
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#==================================================================================================
+# FUNCIONES
+#==================================================================================================
+def polinomial_interpolation(x,y,var_name='m'):
+    vandermonde=np.vander(x,increasing=True)
+    coef=np.dot(np.linalg.inv(vandermonde),y)
+    expression=''
+    for i in range(len(x)):
+        sign=''
+        if coef[i]>=0:
+            sign='+'        
+        expression+=sign+str(coef[i])+'*'+str(var_name)+'**'+str(i)
+
+    return  expression
+
+def draw_expression(expression,x,y,env_name):
+    expression_x=np.arange(min(x),max(x)+(max(x)-min(x))/100,(max(x)-min(x))/100)
+    expression_y=[]
+    for m in expression_x:
+        expression_y.append(eval(expression))
+    
+    plt.figure(figsize=[6,6])
+    ax=plt.subplot(111)
+    plt.plot(expression_x,expression_y,color='blue',label='Interpolation')
+    plt.scatter(x,y,color='red',marker='x',label='Interpolation points')
+    plt.title(str(env_name))
+    ax.set_ylabel('Accuracy')
+    ax.set_xlabel('Evaluation cost')
+    ax.legend()
+    plt.savefig('results/figures/general/interpolation_'+str(env_name)+'.png')
+    plt.show()
+
+#==================================================================================================
+# PROGRAMA PRINCIPAL
+#==================================================================================================
+list_env_names=['SymbolicRegressor','WindFLO','MuJoCo','Turbines']
+df=[]
+
+for env_name in list_env_names:
+    df_acc_eval_cost=pd.read_csv('results/data/'+str(env_name)+'/UnderstandingAccuracy/df_Bisection.csv')
+    expression=polinomial_interpolation(df_acc_eval_cost['cost_per_eval'],df_acc_eval_cost['accuracy'])
+    inverse_expression=polinomial_interpolation(df_acc_eval_cost['accuracy'],df_acc_eval_cost['cost_per_eval'],var_name='m_inv')
+    draw_expression(expression,df_acc_eval_cost['cost_per_eval'],df_acc_eval_cost['accuracy'],env_name)
+    df.append([env_name,expression,inverse_expression,min(df_acc_eval_cost['cost_per_eval']),max(df_acc_eval_cost['cost_per_eval'])])
+
+df=pd.DataFrame(df,columns=['env_name','interpolation_expression','inverse_expression','lower_time','upper_time'])
+df.to_csv('results/data/general/bisection_interval_interpolation.csv')
+
--- experimentScripts_general/sample_size_bisection_method.py
+++ experimentScripts_general/sample_size_bisection_method.py
@@ -5,7 +5,7 @@
 # al coste predefinido.
 
 #==================================================================================================
-# LIBRERIAS
+# LIBRERÍAS
 #==================================================================================================
 import pandas as pd
 import math
@@ -14,70 +14,112 @@ import numpy as np
 #==================================================================================================
 # FUNCIONES
 #==================================================================================================
+def bisection_middle_points(lower,upper,type_cost='max'):
+	list=[] 
+	stop_threshold=(upper-lower)*0.1
+	max_value=upper
+	# Caso intermedio.
+	if type_cost=='mean':
+		first=True
+		while abs(lower-upper)>stop_threshold:       
+			middle=(lower+upper)/2
+			list.append(middle)
+			if first:
+				lower=middle
+				first=False
+			else:
+				upper=middle
+	# En todas las iteraciones se acota el intervalo por arriba (caso más costoso).
+	if type_cost=='max':
+		while abs(lower-upper)>stop_threshold:       
+			middle=(lower+upper)/2
+			list.append(middle)
+			lower=middle
+
+	return list+[max_value]
+
 # Calcular el tamaño de muestra para el método de bisección y la frecuencia con la que se deberá 
 # tener en cuenta las indicaciones del heurístico que nos indica cuando debemos reajustar el accuracy.
-def sample_size_and_frequency(env_name,popsize):
+def sample_size_and_frequency(env_name,popsize,perc_cost,bisection_cost_type='max',only_bisection=True):
 
-	# Leer base de datos con la información de coste por evaluación con accuracys considerados
-	# en el peor caso de una aplicación del método de bisección.
-	df=pd.read_csv('results/data/'+str(env_name)+'/UnderstandingAccuracy/df_BisectionSample.csv',index_col=0)
+	df_acc_eval_cost=pd.read_csv('results/data/'+str(env_name)+'/UnderstandingAccuracy/df_Bisection.csv')
+	time_list=bisection_middle_points(min(df_acc_eval_cost['cost_per_eval']),max(df_acc_eval_cost['cost_per_eval']),type_cost=bisection_cost_type)
 
 	# Coste de evaluar una población con el máximo accuracy.
-	cost_max_acc=list(df['cost_per_eval'])[-1]*popsize
+	cost_max_acc=time_list[-1]*popsize
 
 	# Coste de aplicar el método de bisección sobre una población.
-	cost_bisection=min_sample_size*(sum(list(df['cost_per_eval']))-list(df['cost_per_eval'])[-2])+popsize*list(df['cost_per_eval'])[-2]
+	if only_bisection:
+		cost_bisection=min_sample_size*sum(time_list)
+	else:
+		cost_bisection=min_sample_size*(sum(time_list)-time_list[-2])+popsize*time_list[-2]
 
 	# Porcentaje real.
 	perc=cost_bisection/cost_max_acc
 
 	# Si el aplicar el método de bisección sobrepasa el porcentaje de coste predefinido,
 	# definir la frecuencia con la que se le deberá hacer caso al heurístico (cuando reajustar el accuracy).
-	if perc>perc_total_cost:
+	if perc>perc_cost:
 		sample_size=min_sample_size
-		freq=math.ceil(perc/perc_total_cost)
+		if only_bisection:
+			freq_gen=math.ceil(perc/perc_cost)
+		else:
+			freq_gen=math.ceil((cost_bisection-popsize*time_list[-2])/(perc_cost*cost_max_acc-popsize*time_list[-2]) )
+
 	# En otro caso.
 	else:
-		sample_size=int((perc_total_cost*cost_max_acc-popsize*list(df['cost_per_eval'])[-2])/(sum(list(df['cost_per_eval']))-list(df['cost_per_eval'])[-2]))
-		freq=1
+		if only_bisection:
+			sample_size=int((perc_cost*cost_max_acc)/sum(time_list))
+		else:
+			sample_size=int((perc_cost*cost_max_acc-popsize*time_list[-2])/(sum(time_list)-time_list[-2]))
+		freq_gen=1
+	freq_time=freq_gen*cost_max_acc
 
-	return sample_size,freq
+	return sample_size,freq_gen,freq_time
+
+# Construir base de datos con la información de interés.
+def build_interest_info_df(list_envs_popsize,perc_cost,bisection_cost_type,only_bisection,csv_name):
+	df=[]
+	for env_name in list(list_envs_popsize.keys()):
+		# Tamaño de población.
+		popsize=list_envs_popsize[env_name]
+
+		# Número de generaciones evaluadas con el máximo accuracy en el límite de tiempo prefijado para cada entorno.
+		if env_name=='SymbolicRegressor':
+			df_max_acc=pd.read_csv('results/data/SymbolicRegressor/ConstantAccuracyAnalysis/df_train_acc1.0.csv',index_col=0)
+			n_gen=int(np.mean(list(df_max_acc.groupby('train_seed')['n_gen'].max())))
+		if env_name=='MuJoCo':
+			df_max_acc=pd.read_csv('results/data/MuJoCo/ConstantAccuracyAnalysis/df_ConstantAccuracyAnalysis_acc1.0.csv',index_col=0)
+			n_gen=int(np.mean(list(df_max_acc.groupby('train_seed')['n_gen'].max())))
+		if env_name=='WindFLO':
+			df_max_acc=pd.read_csv('results/data/WindFLO/ConstantAccuracyAnalysis/df_ConstantAccuracyAnalysis1.0.csv',index_col=0)
+			n_gen=int(np.mean(list(df_max_acc.groupby('seed')['n_gen'].max())))
+
+		### FALTA POR INTRODUCIR EL ENTORNO "Turbines"
+
+		# Tamaño de muestra para el método de bisección y frecuencia con la que se ejecutarán las ordenes del heurístico.
+		sample_size,freq_gen,freq_time=sample_size_and_frequency(env_name,list_envs_popsize[env_name],perc_cost,bisection_cost_type=bisection_cost_type,only_bisection=only_bisection)
+
+		# Añadir información a la base de datos.
+		df.append([env_name,popsize,n_gen,sample_size,freq_gen,freq_time,str(int(n_gen/freq_gen))])
+
+	df=pd.DataFrame(df,columns=['env_name','popsize','n_gen_max_acc','sample_size','frequency_gen','frequency_time','max_update_acc'])
+	df.to_csv('results/data/general/sample_size_freq_'+str(csv_name)+'.csv')
 
 #==================================================================================================
 # PROGRAMA PRINCIPAL
 #==================================================================================================
 # Parámetros.
-list_envs_popsize={'SymbolicRegressor':1000,'MuJoCo':20,'WindFLO':50}#,'Turbines':8}
-perc_total_cost=0.8 # Porcentaje del coste por defecto que se desea considerar para el coste de evaluar una población al usar el método de bisección.
+list_envs_popsize={'SymbolicRegressor':1000,'MuJoCo':20,'WindFLO':50}#,'Turbines':20}
+perc_total_cost=0.95 # Porcentaje del coste por defecto que se desea considerar para el coste de evaluar una población al usar el método de bisección.
+perc_bisec_cost=0.05 # Porcentaje del coste por defecto que se desea considerar únicamente para la aplicación del método de bisección.
 min_sample_size=10 # Tamaño mínimo
 
-# Construir base de datos con la información de interés.
-df=[]
-for env_name in list(list_envs_popsize.keys()):
-	# Tamaño de población.
-	popsize=list_envs_popsize[env_name]
+# Construir tabla para en el caso en que se pretende que el coste de aplicar el heurístico en total sea com mucho un 95% del coste por defecto.
+build_interest_info_df(list_envs_popsize,perc_total_cost,'mean',False,'BisectionAndPopulation')
 
-	# Número de generaciones evaluadas con el máximo accuracy en el límite de tiempo prefijado para cada entorno.
-	if env_name=='SymbolicRegressor':
-		df_max_acc=pd.read_csv('results/data/SymbolicRegressor/ConstantAccuracyAnalysis/df_train_acc0.1.csv',index_col=0)
-		n_gen=int(np.mean(list(df_max_acc.groupby('train_seed')['n_gen'].max())))
-	if env_name=='MuJoCo':
-		df_max_acc=pd.read_csv('results/data/MuJoCo/ConstantAccuracyAnalysis/df_ConstantAccuracyAnalysis_acc0.1.csv',index_col=0)
-		n_gen=int(np.mean(list(df_max_acc.groupby('train_seed')['n_gen'].max())))
-	if env_name=='WindFLO':
-		df_max_acc=pd.read_csv('results/data/WindFLO/ConstantAccuracyAnalysis/df_ConstantAccuracyAnalysis0.1.csv',index_col=0)
-		n_gen=int(np.mean(list(df_max_acc.groupby('seed')['n_gen'].max())))
-
-	### FALTA POR INTRODUCIR EL ENTORNO "Turbines"
-
-	# Tamaño de muestra para el método de bisección y frecuencia con la que se ejecutarán las ordenes del heurístico.
-	sample_size,freq=sample_size_and_frequency(env_name,list_envs_popsize[env_name])
-
-	# Añadir información a la base de datos.
-	df.append([env_name,popsize,n_gen,sample_size,freq,int(popsize/freq)])
-
-df=pd.DataFrame(df,columns=['env_name','popsize','n_gen_max_acc','sample_size','frequency','max_update_acc'])
-df.to_csv('results/data/general/bisection_sample_size_heuristic_freq.csv')
+# Construir tabla para en el caso en que se pretende que el coste de aplicar el heurístico en total sea com mucho un 5% más del coste por defecto.
+build_interest_info_df(list_envs_popsize,perc_total_cost,'max',True,'BisectionOnly')
 
 
 	
